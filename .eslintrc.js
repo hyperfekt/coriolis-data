f357d587dff1d812bbb1ca5af6ea01449d7f3951
willyb321 <wbwilliam7@gmail.com>
2018-06-17 08:48:05 +1000

    update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,13 @@
 module.exports = {
-    "env": {},
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [2, 4],
-        "linebreak-style": [2, "unix"],
-        "quotes": [2, "single"],
-        "semi": [2, "always"]
-    }
+  'env': {
+    'browser': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'rules': {
+    'indent': ['error', 2],
+    'linebreak-style': [2, 'unix'],
+    'quotes': [2, 'single'],
+    'semi': [2, 'always']
+  }
 };
